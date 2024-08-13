a8e4507000fedb09b30a0ad8d8b28c14d4cd7a69
Samuel Sml <veramirandasamuel6@gmail.com>
2024-08-13 11:47:35 -0500

    feat: multiplication table (without loops)

--- src/workshop_1/Multiplication_table.java
+++ src/workshop_1/Multiplication_table.java
@@ -1,4 +1,27 @@
 package workshop_1;
 
+import java.util.Scanner;
+
 public class Multiplication_table {
+
+    Scanner sc = new Scanner(System.in);
+    int number;
+
+    public Multiplication_table() {
+        System.out.println("Ingrese el número de la tabla: ");
+        number = Integer.parseInt(sc.nextLine());
+    }
+
+    public void showTable() {
+        System.out.println(number + " x 1= " + (number));
+        System.out.println(number + " x 2 = " + (number * 2));
+        System.out.println(number + " x 3 = " + (number * 3));
+        System.out.println(number + " x 4 = " + (number * 4));
+        System.out.println(number + " x 5 = " + (number * 5));
+        System.out.println(number + " x 6 = " + (number * 6));
+        System.out.println(number + " x 7 = " + (number * 7));
+        System.out.println(number + " x 8 = " + (number * 8));
+        System.out.println(number + " x 9 = " + (number * 9));
+        System.out.println(number + " x 10 = " + (number * 10));
+    }
 }
