b5f5774f7b5edaba1530a9a348ad2fa2ac624b22
Samuel Sml <veramirandasamuel6@gmail.com>
2024-08-13 09:07:14 -0500

    test: check triangle classification

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import workshop_1.Triangle;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Triangle instance
+        Triangle triangle = new Triangle();
+
+        // Call the functions
+        boolean isEquilateral = triangle.isEquilateral();
+        boolean isIsosceles = triangle.isIsosceles();
+        boolean isScalene = triangle.isScalene();
+
+        // Show results
+        System.out.println("Is Equilateral: " + isEquilateral);
+        System.out.println("Is Isosceles: " + isIsosceles);
+        System.out.println("Is Scalene: " + isScalene);
     }
 }
