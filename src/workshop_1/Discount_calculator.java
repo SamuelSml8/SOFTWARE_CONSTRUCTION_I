2060125d7706350c194bb715f8ace9011658de53
Samuel Sml <veramirandasamuel6@gmail.com>
2024-08-13 11:30:15 -0500

    feat: Discount Calculator

--- src/workshop_1/Discount_calculator.java
+++ src/workshop_1/Discount_calculator.java
@@ -1,4 +1,22 @@
 package workshop_1;
 
+import java.util.Scanner;
+
 public class Discount_calculator {
+
+    Scanner sc = new Scanner(System.in);
+    float price, discount;
+
+    public Discount_calculator() {
+        System.out.println("Precio original: ");
+        price = Float.parseFloat(sc.nextLine());
+
+        System.out.println("Porcentaje de descuento: ");
+        discount = Float.parseFloat(sc.nextLine());
+    }
+
+    public void finalPrice() {
+        float discountPrice = price - (price * (discount / 100));
+        System.out.println("El precio final es: " + discountPrice);
+    }
 }
