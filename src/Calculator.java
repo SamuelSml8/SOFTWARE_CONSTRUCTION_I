ddfc668f1b4531c4269ad71869d9a43ee9f8f3db
Samuel Sml <veramirandasamuel6@gmail.com>
2024-08-14 17:04:41 -0500

    feat: add calculator class with methods for addition and subtraction

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    public float sum(float num1, float num2) {
+        return num1 + num2;
+    }
+
+    public float substract(float num1, float num2) {
+        return num1 - num2;
+    }
 }
