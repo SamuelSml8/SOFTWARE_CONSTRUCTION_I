a483d1ee7cc11e29ee613d9bed8fe775217aec13
Samuel Sml <veramirandasamuel6@gmail.com>
2024-09-09 18:16:26 -0500

    clean branch to workshop

--- src/Calculator.java
+++
@@ -1,9 +0,0 @@
-public class Calculator {
-    public float sum(float num1, float num2) {
-        return num1 + num2;
-    }
-
-    public float substract(float num1, float num2) {
-        return num1 - num2;
-    }
-}
