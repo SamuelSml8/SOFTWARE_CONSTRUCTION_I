72865c2fa6bb641981352425cbd5fcaf5ad31d18
Samuel Sml <veramirandasamuel6@gmail.com>
2024-09-11 11:40:57 -0500

    feat: improve ID generation in Helpers class
    - created  method to ensure generated IDs are positive and within the range [0, 9999].

--- src/Helpers/Helpers.java
+++ src/Helpers/Helpers.java
@@ -1,2 +1,11 @@
-package Helpers;public class Helpers {
+package Helpers;
+
+import java.util.Random;
+
+public class Helpers {
+    Random random = new Random();
+    public int generateId() {
+        return Math.abs(random.nextInt(10000));
+    }
+
 }
