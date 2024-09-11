56ae369dff6e023c3b6c9a92417b38aa6c200c14
Samuel Sml <veramirandasamuel6@gmail.com>
2024-09-11 11:42:23 -0500

    feat: enhance Task class with toString method
    - Added a  method to the Task class to improve readability and debugging by providing a detailed string representation of the Task object.

--- src/Models/Task.java
+++ src/Models/Task.java
@@ -1,2 +1,56 @@
-package Models;public class Task {
+package Models;
+
+public class Task {
+    private Integer id;
+    private String title;
+    private String description;
+    private boolean status;
+
+    public Task(String title, String description, boolean status) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }
