0f3b045068413131de906aa4cc2cad08e597f49c
Samuel Sml <veramirandasamuel6@gmail.com>
2024-09-11 11:42:42 -0500

    feat: implement command-line interface for task management
    - Added a command-line interface to interact with the  class.
    - Implemented options to add new tasks, mark tasks as completed, view pending tasks, and exit the application.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import Models.Task;
 import workshop_2.GradeAverage;
+import workshop_2.TaskManagement;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         // Calculation of grade point average
 
@@ -11,6 +16,42 @@ public class Main {
 
         // Call the function and show it
         System.out.println(myGrade.status());
+
+        // Task list management
+        TaskManagement myTaskList = new TaskManagement();
+
+        while (true) {
+            System.out.println("\nGESTIÓN DE TAREAS\n");
+            System.out.println("1. Agregar nueva tarea");
+            System.out.println("2. Marcar tarea como completada");
+            System.out.println("3. Tareas pendientes");
+            System.out.println("4. Salir\n");
+            System.out.println("Opción deseada: ");
+            String option = sc.nextLine();
+
+            if (option.equals("1")) {
+                System.out.println("\nAGREGAR TAREA");
+                System.out.println("\nTítulo: ");
+                String title = sc.nextLine();
+                System.out.println("\nDescripción: ");
+                String description = sc.nextLine();
+                Task task = new Task(title, description, false);
+                System.out.println(myTaskList.addTask(task));
+            } else if (option.equals("2")) {
+                System.out.println("\nMARCAR TAREA COMPLETADA");
+                System.out.println("\nID de la tarea: ");
+                int id = Integer.parseInt(sc.nextLine());
+                System.out.println(myTaskList.taskDone(id));
+            } else if (option.equals("3")) {
+                System.out.println("\nTAREAS PENDIENTES");
+                System.out.println(myTaskList.getPendingTasks());
+            } else if (option.equals("4")) {
+                System.out.println(myTaskList.exit());
+                break;
+            } else {
+                System.out.println("\nOpción inválida");
+            }
+        }
     }
 
 }
